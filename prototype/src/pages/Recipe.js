0c2aaa111d1f18a1696e7e0d455e722ee6d2356b
AlvisGit <alviszou@bu.edu>
2023-04-25 14:21:08 -0400

    added youtube api

--- prototype/src/pages/Recipe.js
+++ prototype/src/pages/Recipe.js
@@ -5,10 +5,19 @@ import { useParams } from 'react-router-dom';
 function Recipe() {
   const { rid } = useParams();
   const [recipe, setRecipe] = useState(null);
+  const [videoId, setVideoId] = useState(null);
   useEffect(() => {
     axios.get(`http://localhost:8000/api/recipes/${rid}`)
       .then(response => {
         setRecipe(response.data);
+        axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${encodeURIComponent(response.data.title)}&key=AIzaSyDYj8nEVypQPyWGz9sXs12nAhUGDhwShGI`)
+          .then(response => {
+            console.log(response);
+            setVideoId(response.data.items[0].id.videoId);
+          })
+          .catch(error => {
+            console.log(error);
+          });
       })
       .catch(error => {
         console.log(error);
@@ -30,8 +39,15 @@ function Recipe() {
       </ul>
       <h2>Instructions:</h2>
       <div dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
+      {videoId && (
+        <div>
+          <h2>Video:</h2>
+          <iframe width="560" height="315" src={`https://www.youtube.com/embed/${videoId}`} title={recipe.title} allowFullScreen></iframe>
+        </div>
+      )}
     </div>
   );
 }
+
 
 export default Recipe;
